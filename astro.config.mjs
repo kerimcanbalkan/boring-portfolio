ef65c0490e3cacd541b2487f8f17fbbde7e48653
kerimcanbalkan <kerimcanbalkan@gmail.com>
2024-12-02 19:02:56 +0300

    deploy action fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,10 @@ import compress from "astro-compress";
 import icon from "astro-icon";
 import { defineConfig } from 'astro/config';
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon(), compress()],
-  output: "server",
-  adapter: vercel(),
+  output: "static",
   site: 'https://kerimcanbalkan.github.io',
   base: '/boring-portfolio'
 });
