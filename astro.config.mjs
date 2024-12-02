2b95e5d06d09cfd8c9e8fab67024c8e0284f90b0
kerimcanbalkan <kerimcanbalkan@gmail.com>
2024-12-02 19:10:16 +0300

    base changed

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,5 @@ export default defineConfig({
   integrations: [tailwind(), icon(), compress()],
   output: "static",
   site: 'https://kerimcanbalkan.github.io',
-  base: '/boring-portfolio'
+  base: '/'
 });
